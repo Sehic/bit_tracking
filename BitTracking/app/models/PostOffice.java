4934040a29520621faec3d61e9934319d6a002c7
mladen91 <mladen.teofilovic@bitcamp.ba>
2015-09-19 11:44:33 +0200

    repair

--- BitTracking/app/models/PostOffice.java
+++ BitTracking/app/models/PostOffice.java
@@ -1,9 +1,9 @@
 package models;
 
+import com.avaje.ebean.Model;
 import com.avaje.ebean.Model.*;
 import helpers.StatusHelper;
 import play.data.validation.Constraints;
-import play.db.ebean.Model;
 
 import javax.persistence.*;
 import java.util.ArrayList;
@@ -14,6 +14,7 @@ import java.util.List;
  */
 @Entity
 public class PostOffice extends Model {
+
     @Id
     public Long id;
     @Column
